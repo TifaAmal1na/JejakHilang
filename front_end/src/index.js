d940420aa36515facbf511ab33ceb6b19f32b14b
Reswita Audinia <reswitaaudinia01@gmail.com>
2024-12-04 01:38:08 +0700

    drawer

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,5 +1,5 @@
 // Mengimpor file CSS
-import '../assets/style.css';  // Sesuaikan path relatif menuju folder assets
+import '../assets/style.css'; // Sesuaikan path relatif menuju folder assets
 
 // Mengimpor modul yang diperlukan
 import loadHome from '../pages/home.js';
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const homeLink = document.getElementById('home-link');
   const formLink = document.getElementById('form-link');
   const mainContent = document.getElementById('main-content');
+  const hamburger = document.getElementById('hamburger');
+  const drawer = document.getElementById('drawer');
+  const closeDrawer = document.getElementById('closeDrawer');
 
   // Tampilkan halaman home saat pertama kali membuka halaman
   loadHome();
@@ -18,11 +21,23 @@ document.addEventListener('DOMContentLoaded', () => {
   homeLink.addEventListener('click', (event) => {
     event.preventDefault(); // Mencegah reload halaman
     loadHome(); // Memanggil fungsi dari home.js untuk menampilkan halaman home
+    drawer.classList.remove('active'); // Tutup menu drawer setelah klik
   });
 
   // Event listener untuk link Form
   formLink.addEventListener('click', (event) => {
     event.preventDefault(); // Mencegah reload halaman
     loadForm(); // Memanggil fungsi dari form.js untuk menampilkan form pelaporan
+    drawer.classList.remove('active'); // Tutup menu drawer setelah klik
+  });
+
+  // Event listener untuk membuka menu navigasi (drawer)
+  hamburger.addEventListener('click', () => {
+    drawer.classList.add('active'); // Tambahkan kelas untuk menampilkan drawer
+  });
+
+  // Event listener untuk menutup menu navigasi (drawer)
+  closeDrawer.addEventListener('click', () => {
+    drawer.classList.remove('active'); // Hapus kelas untuk menyembunyikan drawer
   });
 });
